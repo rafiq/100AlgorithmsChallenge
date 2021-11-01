2c8b2a5567da8fcf26c614b66553a84469820fc4
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-11-01 19:50:17 +0300

    Update absoluteValuesSumMinization.ts

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,19 @@
 export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+    // let min = Infinity;
+
+    // for (let i = 0; i < a.length; i++) {
+    //     for (let j = i + 1; j < a.length; j++) {
+    //         let curr = Math.abs(a[i] - a[j]);
+    //         if (curr < min) min = curr;
+    //     }
+    // }
+    // return min;
+    const isEven = a.length % 2 === 0;
+
+    return isEven ? a[(a.length / 2) - 1] : a[Math.floor(a.length / 2)];q
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
