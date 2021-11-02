e58640a79ec1ad512376094315917a1782617d20
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-11-02 19:41:09 +0300

    Update add.ts

--- add/add.ts
+++ add/add.ts
@@ -1,12 +1,18 @@
 export function add(param1: number, param2: number): number {
-
+    return param1 + param2;
 }
 
 export function add2(...param1: number[]): number {
+    let result = 0;
 
+    for (let num of param1) {
+        result += num;
+    }
+
+    return result;
 }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+console.log(add(1, 2));
+console.log(add(3, 2));
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
