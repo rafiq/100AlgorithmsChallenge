492a5849f02bc1471ee049d9e480b81af0cccb31
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-11-02 19:41:02 +0300

    Update addBorder.ts

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,15 @@
 export function addBorder(picture: string[]): string[] {
+    let rectangleWidth = picture[0].length + 2;
+    let result = [];
+    let stars = "*".repeat(rectangleWidth)
 
+    for (let i = 0; i < picture.length; i++) {
+            result.push("*" + picture[i] + "*");
+    }
+
+    result.unshift(stars);
+    result.push(stars);
+    return result;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
